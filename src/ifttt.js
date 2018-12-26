376f40a0df187dc8c0b29026b42f00bff674f436
wcrbrm <viclopata@gmail.com>
2018-12-26 09:31:56 +0200

    prevention of repeating same command

--- src/ifttt.js
+++ src/ifttt.js
@@ -1,10 +1,12 @@
 const axios = require("axios");
 const createDebug = require('debug');
 
-const done = () => (new Promise(resolve => (resolve())));
+const done = (msg) => (new Promise(resolve => (resolve(msg))));
 const doneWithError = (msg) => (new Promise((resolve, reject) => (reject(msg))));
 
 let warnedOnce = false;
+let lastChange = null;
+
 const sendWebhook = ({ target }) => {
   const dbg = createDebug('ifttt');
   const key = process.env.IFTTT_WEBHOOK_KEY;
@@ -15,9 +17,13 @@ const sendWebhook = ({ target }) => {
     }
     return doneWithError("No IFTTT_WEBHOOK_KEY");
   }
+  if (lastChange === target) {
+    return done(`Change to ${target}*C was already sent`);
+  }
   const eventId = `temperature_${target}c`;
   const url = `https://maker.ifttt.com/trigger/${eventId}/with/key/${key}`;
   dbg(`Posting to ${url}`);
+  lastChange = target;
   const options = { url, method: 'POST' };
   return axios(options)
     .then(response => (response.data))
